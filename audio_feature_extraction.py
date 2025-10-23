6132a0c522236e3a18024e50c35cc9bfe33c4533
Justin <justingr03@gmail.com>
2025-10-23 14:33:24 +0200

    first version of audio feature extraction

---
+++ audio_feature_extraction.py
@@ -0,0 +1,12 @@
+import librosa, torch, numpy
+
+
+
+
+#Feature extraction (maybe?)
+y, sr = librosa.load('audio.wav', sr=16000)
+mel = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
+mel_db = librosa.power_to_db(mel)
+print(type(mel_db))
+print(mel_db.shape)
+
